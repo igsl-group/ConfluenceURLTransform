814ff2dba7f618764abcf767c4224fd2e10769d1
KC Wong <kc.wong@igsl-group.com>
2024-05-08 10:38:34 +0800

    - Removed import statements for Apache Commons CLI.

--- src/main/java/com/igsl/TinyURLGenerator.java
+++ src/main/java/com/igsl/TinyURLGenerator.java
@@ -9,14 +9,6 @@ import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-
 /**
  * Logic of Confluence tiny URL generation:
  * https://confluence.atlassian.com/confkb/how-to-programmatically-generate-the-tiny-link-of-a-confluence-page-956713432.html
